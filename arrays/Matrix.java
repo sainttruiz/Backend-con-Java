301c06a42e57d91de81b8af713fac9d8552040c6
sainttruiz <sainttruiz@gmail.com>
2024-10-19 11:06:07 -0500

    Update Matrix.java: Constructor and fill() method created.

--- arrays/Matrix.java
+++ arrays/Matrix.java
@@ -1,3 +1,16 @@
 package arrays;
+import java.util.Scanner;
+import java.util.Arrays;
 
-public class Matrix{}
+public class Matrix{
+    public int[][] matrix1;
+    public int[][] matrix2;
+
+    public Matrix(int i, int j, int k, int l){
+        this.matrix1 = new int[i][j];
+        this.matrix2 = new int[k][l];
+
+    }
+    public void fill(Scanner sc){}
+
+}
